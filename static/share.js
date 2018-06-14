18a235c2d2286f449c862975afe90f9e651085d9
ZinMarKyawww <ZinMarKyaw5550@gmail.com>
2018-06-14 11:09:31 +0630

    added index.html and files in staic folder

---
+++ static/share.js
@@ -0,0 +1,30 @@
+
+// pwa fire for developers - codelab - web share api - add on
+// https://pwafire.org/developer/
+// Authored by maye edwin https://twitter.com/MayeEdwin1
+// ths web app share add on works magically when on your mobile device;
+
+(function () {
+    'use strict';
+    // check if share API is supported or not
+    if (navigator.share !== undefined) {
+      document.addEventListener('DOMContentLoaded', function() {
+        var shareBtn = document.querySelector('.share');
+        // add share button event listener
+        shareBtn.addEventListener('click', function(event) {
+          // web share API
+          navigator.share({
+            title: document.title,
+            text: 'This is PWA Fire Demo Progressive App #pwafire #pwafirebuild #MeetMaye',
+            url: window.location.href
+          })
+          .then(function() {
+            console.info('PWA Fire Demo shared successfully!');
+          })
+          .catch(function (error) {
+            console.error('Wooooooo! Some magic failed in sharing: ', error);
+          })
+        });
+      });
+    }
+  })();
